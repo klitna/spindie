5cc2e3c7aba78de5c959baf78d3f4009871391ff
Iryna <cf19iryna.klitna@iesjoandaustria.org>
2021-01-27 18:34:30 +0100

    exception not handled

--- app/src/main/java/com/example/spindie/MainActivity.java
+++ app/src/main/java/com/example/spindie/MainActivity.java
@@ -2,7 +2,19 @@ package com.example.spindie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
+
+        //if not authenticated, then register through google
+        FirebaseAuth myAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = myAuth.getCurrentUser();
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if(user!=null) {
+                    //Go to menu Fragment
+                }
+                else{
+                    Intent goToRegister = new Intent(getApplicationContext(), SignInActivity.class);
+                    startActivity(goToRegister);
+                }
+            }
+        }, 2000);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
     }
 }
