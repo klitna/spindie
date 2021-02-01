e8b33f654d1ded2c1c4b8c8fe0721fbf77fc8bc0
Iryna <cf19iryna.klitna@iesjoandaustria.org>
2021-02-01 18:43:47 +0100

    Revert "New Fragments and navigation view"

--- app/src/main/java/com/example/spindie/MainActivity.java
+++ app/src/main/java/com/example/spindie/MainActivity.java
@@ -1,23 +1,13 @@
 package com.example.spindie;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.view.MenuItem;
 
-import com.example.spindie.Fragments.HomeFragment;
-import com.example.spindie.Fragments.MoreFragment;
-import com.example.spindie.Fragments.SearchFragment;
-import com.example.spindie.Fragments.SettingsFragment;
-import com.example.spindie.Fragments.StoreFragment;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -27,7 +17,6 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,45 +43,5 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }, 2000);
-
-        // Configure Google Sign In
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        bottomNavigation = findViewById(R.id.navigationView);
-        openFragment(HomeFragment.newInstance("", ""));
-    }
-
-    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    switch (item.getItemId()) {
-                        case R.id.navigation_home:
-                            openFragment(HomeFragment.newInstance("", ""));
-                            return true;
-                        case R.id.navigation_search:
-                            openFragment(SearchFragment.newInstance("", ""));
-                            return true;
-                        case R.id.navigation_store:
-                            openFragment(StoreFragment.newInstance("", ""));
-                            return true;
-                        case R.id.navigation_settings:
-                            openFragment(SettingsFragment.newInstance("", ""));
-                            return true;
-                        case R.id.navigation_more:
-                            openFragment(MoreFragment.newInstance("", ""));
-                            return true;
-                    }
-                    return false;
-                }
-            };
-
-    public void openFragment(Fragment fragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.mainFragment, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
     }
 }
