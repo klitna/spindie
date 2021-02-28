fcbb01f3664aa12fa97275e2b7a7a36808660bbf
Iryna <cf19iryna.klitna@iesjoandaustria.org>
2021-02-28 15:48:04 +0100

    everything ok

--- app/src/main/java/com/example/spindie/Model/Entities/Film/Film.java
+++ app/src/main/java/com/example/spindie/Model/Entities/Film/Film.java
@@ -1,4 +1,116 @@
 package com.example.spindie.Model.Entities.Film;
 
 public class Film {
+    private String productionDate;
+    private String imgUrl;
+    private String videoUrl;
+    private String id;
+
+    private String titleEN;
+    private String descriptionEN;
+    private String directorEN;
+    private String actorsEN;
+
+    private String titleES;
+    private String descriptionES;
+    private String directorES;
+    private String actorsES;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getActorsES() {
+        return actorsES;
+    }
+
+    public String getTitleES() {
+        return titleES;
+    }
+
+    public String getTitleEN() {
+        return titleEN;
+    }
+
+    public String getDescriptionES() {
+        return descriptionES;
+    }
+
+    public String getDirectorES() {
+        return directorES;
+    }
+
+    public String getDescriptionEN() {
+        return descriptionEN;
+    }
+
+    public String getDirectorEN() {
+        return directorEN;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getProductionDate() {
+        return productionDate;
+    }
+
+    public String getActorsEN() {
+        return actorsEN;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    //-------------------------------------------------------------
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitleEN(String titleEN) {
+        this.titleEN = titleEN;
+    }
+
+    public void setTitleES(String titleES) {
+        this.titleES = titleES;
+    }
+
+    public void setActorsEN(String actorsEN) {
+        this.actorsEN = actorsEN;
+    }
+
+    public void setActorsES(String actorsES) {
+        this.actorsES = actorsES;
+    }
+
+    public void setDescriptionEN(String descriptionEN) {
+        this.descriptionEN = descriptionEN;
+    }
+
+    public void setDescriptionES(String descriptionES) {
+        this.descriptionES = descriptionES;
+    }
+
+    public void setDirectorEN(String directorEN) {
+        this.directorEN = directorEN;
+    }
+
+    public void setDirectorES(String directorES) {
+        this.directorES = directorES;
+    }
+
+    public void setProductionDate(String productionDate) {
+        this.productionDate = productionDate;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
 }
---
+++ app/src/main/java/com/example/spindie/Model/Entities/Film/ReadWriteFilm.java
@@ -0,0 +1,53 @@
+package com.example.spindie.Model.Entities.Film;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.example.spindie.Model.Entities.User.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReadWriteFilm {
+    private static final String TAG = "ReadAndWriteFilm";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference usersRef;
+    public ReadWriteFilm(DatabaseReference database) {
+        mDatabase = database;
+        usersRef = mDatabase.child("users");
+    }
+
+    public void writeNewFilmES(Film film) {
+        // Create a new user with a first and last name
+        Map<String, Object> newFilm = new HashMap<>();
+        newFilm.put("title", film.getTitleES());
+        newFilm.put("description", film.getDescriptionES());
+        newFilm.put("actors", film.getActorsES());
+        newFilm.put("id", film.get());
+
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+    }
+}
